92143813685e88266abfaeca68c9b6f575f37ef5
Only-Java <javabackendeveloper@gmail.com>
2024-08-05 10:38:06 +0500

    first commit

---
+++ src/main/java/com/example/edusystem/EduSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.edusystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/edusystem/EduSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edusystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
